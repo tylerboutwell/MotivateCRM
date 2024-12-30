cc018e55d0333f682d5d5236fc0fa4496523df17
Tyler Boutwell <tylerboutwell1@gmail.com>
2024-12-30 17:56:26 -0600

    add app to project settings installed_apps

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hello_app.apps.HelloAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
