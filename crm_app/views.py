fb72192cd5936f31f81652708c74e437a0d014cd
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-01-01 18:21:13 -0600

    update recent_orders template

--- crm_app/views.py
+++ crm_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.template import loader
+
 from .models import Transaction
 
 
@@ -13,8 +14,7 @@ def detail(request, user_id):
 
 def recent_orders(request):
     latest_order_list = Transaction.objects.order_by("transaction_datetime")[:5]
-    template = loader.get_template("crm\recent_orders.html")
     context = {
-        "latest_order_list": latest_order_list
+        "latest_order_list": latest_order_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "crm/recent_orders.html", context)
