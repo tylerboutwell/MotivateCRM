353e49c86e4d344a930b73039ea1b95444dee1c5
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-01-01 19:24:04 -0600

    add new view for customer info

--- crm_app/urls.py
+++ crm_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
 
     path("<int:transaction_id>/", views.detail, name="detail"),
 
-    path("recent_orders/", views.recent_orders, name="recent orders")
+    path("recent_orders/", views.recent_orders, name="recent orders"),
+
+    path("customer/<int:customer_id>/", views.customer_detail, name="customer detail")
 ]
--- crm_app/views.py
+++ crm_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import Transaction
+from .models import Transaction, Customer
 
 
 def index(request):
@@ -12,6 +12,10 @@ def index(request):
 def detail(request, transaction_id):
     transaction = get_object_or_404(Transaction, pk=transaction_id)
     return render(request, "crm/detail.html", {"transaction": transaction})
+
+def customer_detail(request, customer_id):
+    customer = get_object_or_404(Customer, pk=customer_id)
+    return render(request, "crm/customer_detail.html", {"customer": customer})
 
 def recent_orders(request):
     latest_order_list = Transaction.objects.order_by("transaction_datetime")[:5]
