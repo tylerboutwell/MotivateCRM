21c824b3df10ff0264896b2ce56b280af10afaa4
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-01-04 08:46:00 -0600

    edit admin file to show date before total in transactions

--- crm_app/admin.py
+++ crm_app/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
 from .models import Customer, Transaction
+
+class TransactionAdmin(admin.ModelAdmin):
+    fields = ["customer", "transaction_datetime", "total"]
+
+
+admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(Customer)
-admin.site.register(Transaction)
