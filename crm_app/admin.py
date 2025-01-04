1620767b32c024ff3308474cfb5a65763722f9d5
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-01-04 16:11:08 -0600

    change admin display -add filter option for transactions -add search fields

--- crm_app/admin.py
+++ crm_app/admin.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 
 from .models import Customer, Transaction
 
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["name", "phone_number", "email"]
+    search_fields = ["phone_number", "email"]
+
 class TransactionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["customer"]}),
         ("Customer Information", {"fields": ["transaction_datetime", "total"]})
         ]
+    list_display = ["customer", "total", "transaction_datetime"]
+    list_filter = ["transaction_datetime"]
 
 
 admin.site.register(Transaction, TransactionAdmin)
-admin.site.register(Customer)
+admin.site.register(Customer, CustomerAdmin)
