ac33a517849d50367d48a3afe4a6357196e83871
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-01-04 08:53:08 -0600

    seperate customer and customer information in transaction model in admin.py

--- crm_app/admin.py
+++ crm_app/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from .models import Customer, Transaction
 
 class TransactionAdmin(admin.ModelAdmin):
-    fields = ["customer", "transaction_datetime", "total"]
+    fieldsets = [
+        (None, {"fields": ["customer"]}),
+        ("Customer Information", {"fields": ["transaction_datetime", "total"]})
+        ]
 
 
 admin.site.register(Transaction, TransactionAdmin)
