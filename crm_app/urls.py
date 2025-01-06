0767576c37934bada8a6b89bb68a7d0e4e247453
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-01-06 15:45:57 -0600

    add views directory to start seperating them

--- crm_app/urls.py
+++ crm_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from . import views
+from .views import views
 
 app_name = "crm_app"
 urlpatterns = [
--- crm_app/views/views.py
+++ crm_app/views/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 from django.views import generic
-from .models import Transaction, Customer
+from ..models import Transaction, Customer
 from django.utils import timezone
 
 
