ef0224a9468d7fb91c227c12554a4c40e979949c
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-01-07 18:20:26 -0600

    add table of customers to home page if user logged in

--- crm_app/views/user_views.py
+++ crm_app/views/user_views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from ..models import Customer
 
 def home(request):
+    customers = Customer.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -15,13 +17,11 @@ def home(request):
             messages.success(request, "There was an error logging in. Please try again...")
             return redirect('crm_app:home')
     else:
-        return render(request, 'crm/home.html', {})
+        return render(request, 'crm/home.html', {'customers':customers})
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out.")
     return redirect('crm_app:home')
 
-def register(request):
-    pass
 
