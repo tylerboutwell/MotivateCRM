36ae41667ea8f1d386c6391768babcffb013ffc0
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-01-13 19:30:38 -0600

    customize add transaction form

--- crm_app/forms.py
+++ crm_app/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from .models import Customer, Transaction
+from django.utils import timezone
 
 class AddCustomerForm(forms.ModelForm):
     name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Name", "class": "form-control"}), label="")
@@ -13,6 +14,10 @@ class AddCustomerForm(forms.ModelForm):
         exclude = ("user",)
 
 class AddTransactionForm(forms.ModelForm):
+    customer = forms.ModelChoiceField(queryset=Customer.objects.all(), required=True,empty_label="--Select Customer--", label="")
+    total = forms.IntegerField(required=True, widget=forms.widgets.NumberInput(attrs={"placeholder": "Total", "class": "form-control"}), label="")
+    transaction_datetime = forms.DateTimeField(required=True, widget=forms.widgets.DateTimeInput(attrs={"type": "datetime-local", "class": "form-control"}), initial=timezone.now(),label="")
+
     class Meta:
         model = Transaction
         exclude = ("user",)
