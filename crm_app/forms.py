41310be97e682cb93e853f3729a750f4de95a462
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-01-19 15:41:55 -0600

    make form horizontal

--- crm_app/forms.py
+++ crm_app/forms.py
@@ -3,20 +3,80 @@ from django.contrib.auth.models import User
 from django import forms
 from .models import Customer, Transaction
 from django.utils import timezone
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit
 
 class AddCustomerForm(forms.ModelForm):
-    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "First Name", "class": "form-control", 'style': 'width: 300px; display: inline',}), label="First Name",)
-    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Last Name", "class": "form-control", 'style': 'width: 300px; display: inline',}), label="Last Name")
-    phone_number = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Phone Number", "class": "form-control", 'style': 'width: 300px; display: inline',}), label="Phone number")
-    email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Email", "class": "form-control", 'style': 'width: 300px; display: inline'}), label="Email address")
-    address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Address", "class": "form-control", 'style': 'width: 300px; display: inline'}), label="Address line")
-    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "City", "class": "form-control", 'style': 'width: 300px; display: inline'}), label="City")
-    state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "State", "class": "form-control", 'style': 'width: 300px; display: inline'}), label="State")
-    postal_code = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Postal Code", "class": "form-control", 'style': 'width: 300px; display: inline'}), label="Postal Code")
+    first_name = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(attrs={"placeholder": "First Name", "class": "form-control",}),
+        label="First Name",
+        )
+    
+    last_name = forms.CharField(
+        required=True, 
+        widget=forms.widgets.TextInput(attrs={"placeholder": "Last Name", "class": "form-control",}), 
+        label="Last Name"
+        )
+    
+    phone_number = forms.CharField(
+        required=True, 
+        widget=forms.widgets.TextInput(attrs={"placeholder": "Phone Number", "class": "form-control",}), 
+        label="Phone number"
+        )
+    
+    email = forms.CharField(
+        required=True, 
+        widget=forms.widgets.TextInput(attrs={"placeholder": "Email", "class": "form-control",}), 
+        label="Email address"
+        )
+    
+    address = forms.CharField(
+        required=True, 
+        widget=forms.widgets.TextInput(attrs={"placeholder": "Address", "class": "form-control",}), 
+        label="Address line"
+        )
+    
+    city = forms.CharField(
+        required=True, 
+        widget=forms.widgets.TextInput(attrs={"placeholder": "City", "class": "form-control",}), 
+        label="City"
+        )
+    
+    state = forms.CharField(
+        required=True, 
+        widget=forms.widgets.TextInput(attrs={"placeholder": "State", "class": "form-control",}), 
+        label="State"
+        )
+    
+    postal_code = forms.CharField(
+        required=True, 
+        widget=forms.widgets.TextInput(attrs={"placeholder": "Postal Code", "class": "form-control",}), 
+        label="Postal Code"
+        )
+    
 
     class Meta:
         model = Customer
         exclude = ("user",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+
+        self.helper.form_class= 'form-horizontal'
+        self.helper.label_class='col-lg-2'
+        self.helper.field_class = 'col-lg-8'
+        self.helper.layout = Layout(
+            'first_name',
+            'last_name',
+            'phone_number',
+            'email',
+            'address',
+            'city',
+            'state',
+            'postal_code',
+        )
 
 class AddTransactionForm(forms.ModelForm):
     customer = forms.ModelChoiceField(queryset=Customer.objects.all(), required=True,empty_label="-- Select Customer --", label="")
