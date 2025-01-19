3ea28473528bbedba11c622361446af2b033f81b
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-01-19 13:43:34 -0600

    move labels to side

--- crm_app/forms.py
+++ crm_app/forms.py
@@ -5,14 +5,14 @@ from .models import Customer, Transaction
 from django.utils import timezone
 
 class AddCustomerForm(forms.ModelForm):
-    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "First Name", "class": "form-control", 'style': 'width: 300px; display: inline',}), label="First Name")
+    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "First Name", "class": "form-control", 'style': 'width: 300px; display: inline',}), label="First Name",)
     last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Last Name", "class": "form-control", 'style': 'width: 300px; display: inline',}), label="Last Name")
-    phone_number = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Phone Number", "class": "form-control", 'style': 'width: 300px',}), label="Phone number")
-    email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Email", "class": "form-control"}), label="Email address")
-    address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Address", "class": "form-control"}), label="Address line")
-    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "City", "class": "form-control"}), label="City")
-    state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "State", "class": "form-control"}), label="State")
-    postal_code = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Postal Code", "class": "form-control"}), label="Postal Code")
+    phone_number = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Phone Number", "class": "form-control", 'style': 'width: 300px; display: inline',}), label="Phone number")
+    email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Email", "class": "form-control", 'style': 'width: 300px; display: inline'}), label="Email address")
+    address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Address", "class": "form-control", 'style': 'width: 300px; display: inline'}), label="Address line")
+    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "City", "class": "form-control", 'style': 'width: 300px; display: inline'}), label="City")
+    state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "State", "class": "form-control", 'style': 'width: 300px; display: inline'}), label="State")
+    postal_code = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder": "Postal Code", "class": "form-control", 'style': 'width: 300px; display: inline'}), label="Postal Code")
 
     class Meta:
         model = Customer
