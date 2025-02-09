d34f0ad70f2634e8dcfd2b4def97483727e85cbe
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-02-08 18:25:55 -0600

    change app urls to root of project urls

--- crm_project/urls.py
+++ crm_project/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
-    path('crm/', include("crm_app.urls")),
+    path('', include("crm_app.urls")),
     path('admin/', admin.site.urls),
 ] + debug_toolbar_urls()
