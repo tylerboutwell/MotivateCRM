910de23a9d50883862fd888a78c01bce2ca1d552
Tyler Boutwell <tylerboutwell1@gmail.com>
2025-02-19 16:28:38 -0600

    create settings folder for different settings

--- crm_project/settings/settings.py
+++ crm_project/settings/settings.py
@@ -20,13 +20,8 @@ env = environ.Env(
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-print("ENV----")
-print(env('SECRET_KEY'))
 
 SECRET_KEY = env("SECRET_KEY", default="change_me")
 
